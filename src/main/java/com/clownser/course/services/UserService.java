c7c75095ec9c6798a6269c31b0338e47312fada0
Clownser <clownser1994@gmail.com>
2020-03-17 10:35:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/clownser/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.clownser.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clownser.course.entities.User;
+import com.clownser.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User>findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
