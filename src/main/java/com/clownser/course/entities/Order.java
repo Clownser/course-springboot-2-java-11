f915215db2496e8be571e5b4e933c437afca2594
Clownser <clownser1994@gmail.com>
2020-06-18 19:11:23 -0300

    OrderItem, many-to-many, assiciation with extra attribuites

--- src/main/java/com/clownser/course/entities/Order.java
+++ src/main/java/com/clownser/course/entities/Order.java
@@ -2,12 +2,16 @@ package com.clownser.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.clownser.course.entities.enums.OrderStatus;
@@ -28,8 +32,12 @@ public class Order implements Serializable {
 	private Integer orderStatus;
 
 	@ManyToOne
+	@JoinColumn(name ="client_id")
 	private User client;
 
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Order() {
 	}
 
@@ -73,6 +81,10 @@ public class Order implements Serializable {
 		if (orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+	
+	public Set<OrderItem> getItems(){
+		return items;
 	}
 
 	@Override
